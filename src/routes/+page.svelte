09223abc7a372763f8cd77c70fd77fc6ce1f9c7c
“NatMaxVal030” <overtakemadaka@gmail.com>
2024-09-16 03:22:17 +0700

    feat: 🎸 test svelte function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	let title: string = 'Natsu';
+	let inputTitle: string = '';
+
+	const changeTitle = () => {
+		title = 'Gray';
+	};
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+	<div class="space-y-5 max-w-96">
+		<h1 class="h1">Let's get cracking bones! {title}</h1>
 		<p>Start by exploring:</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
@@ -11,5 +19,20 @@
 				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
 			</li>
 		</ul>
+		<div>
+			<input type="text" bind:value={title} class="input my-3" />
+			<button on:click={changeTitle} type="button" class="btn variant-filled-surface"
+				>Change title</button
+			>
+			<a href="/mock-route" class="btn variant-filled-surface">Go to mock route</a>
+		</div>
 	</div>
 </div>
+
+<!-- <div>
+	<p>Input title: {inputTitle}</p>
+	<input type="text" bind:value={inputTitle} class="input" />
+</div> -->
+
+
+
